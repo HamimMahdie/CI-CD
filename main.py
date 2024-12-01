77951e91100ae87698a7b3f0778f1f312149a7f9
Hamim Mahdie <hamimmahdie2@gmail.com>
2024-12-01 10:15:54 -0500

    added docker and yaml files

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Hello, CI/CD with GCP and Flask!"})
+    return jsonify({"message": "Docker says- Hello, CI/CD with GCP and Flask!"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
