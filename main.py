49e71f29356678538d13606d7c9bcd3b2580cb1f
Hamim Mahdie <144726390+HamimMahdie@users.noreply.github.com>
2025-06-08 22:56:58 -0400

    PR4

--- main.py
+++ main.py
@@ -3,6 +3,20 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 print("PR3")
+
+
+def tri_recursion(k):
+  if(k>0):
+    result = k+tri_recursion(k-1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("\n\nRecursion Example Results")
+tri_recursion(6)
+
+
 @app.route('/')
 def home():
     return jsonify({"message": "Docker says- Hello, CI/CD with GCP and Flask!"})
